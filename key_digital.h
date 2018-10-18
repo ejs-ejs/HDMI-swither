937c670f5b1a35fa03796cf8d18023533504a20d
ejs <eugenijus.januskevicius@vgtu.lt>
2018-10-18 08:57:10 +0300

    Add files via upload
    Initial commit. Worksforme

---
+++ key_digital.h
@@ -0,0 +1,37 @@
+#ifndef KEY_DIGITAL
+#define KEY_DIGITAL 1
+
+
+// Key Digital HDMI switcher remote KDRMSWPROK; 38 kHz
+
+#define KEY_DIGITAL_PREFIX 0x1819; // Prefix, senior 16 bits
+
+#define KEY_DIGITAL_POWER_ON 0x28D7; // Power on 
+#define KEY_DIGITAL_POWER_OFF 0x629D; // Power off
+
+#define KEY_DIGITAL_INPUT_1 0x9867; // Input 1
+#define KEY_DIGITAL_INPUT_2 0xD827; // Input 2
+#define KEY_DIGITAL_INPUT_3 0x8877; // Input 3
+#define KEY_DIGITAL_INPUT_4 0xA857; // Input 4
+
+#define KEY_DIGITAL_INPUT_R1 0x40BF; // R1
+#define KEY_DIGITAL_INPUT_R2 0x8A75; // R2
+#define KEY_DIGITAL_INPUT_R3 0x58A7; // R3
+
+#define KEY_DIGITAL_AUTO_LEFT 0x0AF5; // Auto Left
+#define KEY_DIGITAL_AUTO_CENTER 0xAA55; // Auto center (blue)
+#define KEY_DIGITAL_AUTO_RIGHT 0x12ED; // Auto Right
+
+
+#define KEY_DIGITAL_DS_1 0xE21D; // Device select 1
+#define KEY_DIGITAL_DS_2 0xE01F; // Device select 2
+#define KEY_DIGITAL_DS_3 0x02FD; // Device select 3
+#define KEY_DIGITAL_DS_4 0x18E7; // Device select 4
+#define KEY_DIGITAL_DS_5 0x22DD; // Device select 5
+#define KEY_DIGITAL_DS_6 0xF00F; // Device select 6
+#define KEY_DIGITAL_DS_7 0x50AF; // Device select 7
+#define KEY_DIGITAL_DS_8 0x7887; // Device select 8
+#define KEY_DIGITAL_DS_9 0x788F; // Device select 9
+#define KEY_DIGITAL_DS_0 0x4AB5; // Device select 0
+
+#endif
